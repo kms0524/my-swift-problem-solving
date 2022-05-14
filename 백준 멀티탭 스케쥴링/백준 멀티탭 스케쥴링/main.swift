2a3d6df41294e6337b0e1003618f1354e909e5c4
kms0524 <gcong2011@gmail.com>
2022-05-14 18:13:05 +0900

    [ADD] 백준 멀티탭 스케줄링 업로드

--- 백준 멀티탭 스케쥴링/백준 멀티탭 스케쥴링/main.swift
+++ 백준 멀티탭 스케쥴링/백준 멀티탭 스케쥴링/main.swift
@@ -5,6 +5,9 @@
 //  Created by 강민성 on 2022/05/11.
 //
 
+/// https://woongsios.tistory.com/139
+/// LRU 알고리즘을 사용해야할듯...?
+
 import Foundation
 
 var NK = readLine()!.split(separator: " ").map { Int($0)! }
@@ -12,32 +15,49 @@ var NK = readLine()!.split(separator: " ").map { Int($0)! }
 var N = NK[0]
 var K = NK[1]
 
-var arr = readLine()!.split(separator: " ").map { Int($0)! }
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+var arr = [Int]()
+var answer = 0
 
-var count = 0
-
-var dic = [Int: Int]()
-
-for i in 0..<arr.count {
-    dic[arr[i]] = 0
-}
-
-for i in 0..<arr.count {
-    dic[arr[i]]! += 1
-}
-
-var remain = N
-
-for i in 0..<arr.count {
-    var current = arr[i]
+for (i, value) in input.enumerated() {
+    if arr.contains(value) {
+        continue
+    }
     
-    if remain > 0 {
-        remain -= 1
-        dic[arr[i]]! -= 1
+    else if arr.count < N {
+        arr.append(value)
     }
     else {
         
+        var count = -1
+        var least = -1
+        
+        for (index, value) in arr.enumerated() {
+            
+            var current = -1
+            
+            for currentIndex in i+1..<input.count {
+                if value == input[currentIndex] {
+                    current = currentIndex
+                    break
+                }
+            }
+            
+            if current == -1 {
+                count = index
+                break
+            }
+            else {
+                if current > least {
+                    least = current
+                    count = index
+                }
+            }
+        }
+        
+        arr[count] = value
+        answer += 1
     }
 }
 
-print(dic)
+print(answer)
