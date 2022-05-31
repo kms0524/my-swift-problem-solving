5c14cc5a3d690238507e5274db627ab7e19f0ef1
kms0524 <gcong2011@gmail.com>
2022-05-31 14:34:20 +0900

    [UPDATE] 백준 평범한 배낭 수정

---
+++ 백준 가장 긴 바이토닉 부분 수열/백준 가장 긴 바이토닉 부분 수열/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  백준 가장 긴 바이토닉 부분 수열
+//
+//  Created by 강민성 on 2022/05/31.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- 백준 평범한 배낭/백준 평범한 배낭/main.swift
+++ 백준 평범한 배낭/백준 평범한 배낭/main.swift
@@ -7,22 +7,35 @@
 
 import Foundation
 
+var NK = readLine()!.split(separator: " ").map { Int($0)! }
 
-/*
- 
- dp[0][
- 
- */
+var N = NK[0]
+var K = NK[1]
 
-let NK = readLine()!.split(separator: " ").map { Int(String($0))! }
-let N = NK[0]
-let K = NK[1]
+var weights = [Int](repeating: 0, count: K + 1)
+var values = [Int](repeating: 0, count: K + 1)
 
-for i in 0..<N {
-    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-    
-    var W = input[0]
-    var V = input[1]
-    
-    
+for i in 1...N {
+    var input = readLine()!.split(separator: " ").map { Int($0)! }
+    weights[i] = input[0]
+    values[i] = input[1]
 }
+
+func knapsack(_ stuffCount: Int, _ bagWeight: Int, _ weights: [Int], _ values: [Int]) -> Int {
+    
+    var dp = [Int](repeating: 0, count: bagWeight + 1)
+    
+    for i in 1...stuffCount {
+        for j in stride(from: bagWeight, to: 0, by: -1) {
+            if weights[i] <= j {
+                dp[j] = max(dp[j], dp[j - weights[i]] + values[i])
+            }
+        }
+    }
+    
+    var maxValue = dp[bagWeight]
+    
+    return maxValue
+}
+
+print(knapsack(N, K, weights, values))
